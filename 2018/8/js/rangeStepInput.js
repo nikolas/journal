5f6682ec048ea2930638a08c6ce85f6e0799154e
Nik Nyby <nnyby@columbia.edu>
2018-08-27 22:50:30 -0400

    updates

--- 2018/8/js/rangeStepInput.js
+++ 2018/8/js/rangeStepInput.js
@@ -1,10 +1,30 @@
 var savedValue = 25;
+var isMouseDown = false;
+var isDragging = false;
 
-function handleRangeInput(input) {
+function onMouseDown() {
+    isMouseDown = true;
+}
+function onMouseUp() {
+    isMouseDown = false;
+    isDragging = false;
+}
+function onMouseMove() {
+    if (isMouseDown) {
+        isDragging = true;
+    }
+}
+
+function onInput(input) {
     var step = new Number(input.step);
     var newVal = new Number(input.value);
     var oldVal = savedValue;
-    if (oldVal) {
+    if (
+        // Disable the oninput filter with the user is dragging
+        // the slider's knob.
+        !(isMouseDown && isDragging) &&
+            oldVal
+    ) {
         input.value = (newVal > oldVal) ?
             oldVal + step : oldVal - step;
     }
